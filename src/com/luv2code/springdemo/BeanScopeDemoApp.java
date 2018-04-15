d763bb297c20cdebd2cd43b119a8db3de28fc382
Jadzka <jarekgolawski19953@gmail.com>
2018-04-15 20:14:41 +0200

    Bean Life Cycle

--- src/com/luv2code/springdemo/BeanScopeDemoApp.java
+++ src/com/luv2code/springdemo/BeanScopeDemoApp.java
@@ -21,9 +21,9 @@ public class BeanScopeDemoApp {
 		// print out the results
 		System.out.println("\nPointing to the same object: " + result);
 		
-		System.out.println("]nMemory location for theCoach: "+ theCoach);
+		System.out.println("\nMemory location for theCoach: "+ theCoach);
 		
-		System.out.println("]nMemory location for alphaCoach: "+ alphaCoach + "\n");
+		System.out.println("\nMemory location for alphaCoach: "+ alphaCoach + "\n");
 		
 		// close the context
 		context.close();
--- src/com/luv2code/springdemo/TrackCoach.java
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -4,8 +4,6 @@ public class TrackCoach implements Coach {
 	
 	private FortuneService fortuneService;
 	
-	
-
 	public TrackCoach(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
 	}
@@ -24,5 +22,16 @@ public class TrackCoach implements Coach {
 		
 		return "Just do it: " + fortuneService.getFortune();
 	}
+	
+	// add and init method
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: inside mthod doMyStartupStuff");
+	}
+	
+	// add a destroy method
+	public void doMyCleanupStuffYoYo() {
+		System.out.println("TrackCoach: inside mthod doMyStartupStuffYoYo");
+	}
+	
 
 }
